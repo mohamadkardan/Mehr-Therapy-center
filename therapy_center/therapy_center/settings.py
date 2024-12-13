f1a9fbe323635a5e3e7b70f4b01923d629abf754
mohammad <marandiandr.com@gmail.com>
2024-12-13 18:50:52 +0330

    chore: store database credentials in virtual environment by decouple package

--- therapy_center/therapy_center/settings.py
+++ therapy_center/therapy_center/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +77,12 @@ WSGI_APPLICATION = 'therapy_center.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mehr_therapy', 
-        'USER': 'admin',
-        'PASSWORD': '13821395',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config("DB_NAME"), 
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST", default='localhost'), 
+        'PORT': config("DB_PORT"),
     }
 }
 
