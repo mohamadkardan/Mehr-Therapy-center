a33c9166f661b339e26e5420b5d38a0ca8352714
Mohammad Mahdi Kardan <mohammadkardan.dev@gmail.com>
2024-12-16 21:16:45 -0800

    chore: Add a default value for role in user model

--- therapy_center/users/models.py
+++ therapy_center/users/models.py
@@ -17,7 +17,7 @@ class User(models.Model):
     ]
 
     phone_number = models.CharField(max_length=11, unique=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='client')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
